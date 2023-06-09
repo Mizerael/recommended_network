5b176367b5ba8c0bb05af846c75a1e54fcd95ffa
Dankoveshnikov <dan-acer@outlook.com>
2023-06-10 00:10:18 +0300

    Добавил рекомендацию на основе жанров и описания

--- app/recomendations.py
+++ app/recomendations.py
@@ -42,37 +42,43 @@ def make_recomendations_with_cf(cf_matrix, name = ""
     except:
         return None
     
+def get_scores(matrix, indexes, name, is_sorted= True):
+    similarity_scores = list(enumerate(matrix[indexes[name]]))
+    if is_sorted:
+        similarity_scores = sorted(similarity_scores
+                                   , key= lambda x: x[1], reverse= True)
+    return list(filter(lambda x: x[0] != indexes[name], similarity_scores))
+
+def vectorization(data, path, kernels):
+    tfidfv = TfidfVectorizer()
+    tfidf_data = tfidfv.fit_transform(data)
+    matrix = kernels(tfidf_data, tfidf_data)
+    if kernels is cosine_similarity:
+        np.save(path, matrix)
+    return matrix
+
 def make_recomendations_with_genre(cbf_matrix, name = ""
                                    , count_recomendations = 10
                                    , kernels= cosine_similarity):
     if os.path.exists(CBF_GENRE_DATA) and kernels is cosine_similarity:
         similarity_matrix = np.load(CBF_GENRE_DATA)
     else:
-        
         genres = cbf_matrix['Genres'].str.split(', ').astype(str)
-        tfidfv = TfidfVectorizer()
-        tfidf_genres = tfidfv.fit_transform(genres)
-        similarity_matrix = kernels(tfidf_genres, tfidf_genres)
-
-        if kernels is cosine_similarity:
-            np.save(CBF_GENRE_DATA, similarity_matrix)
-
+        similarity_matrix = vectorization(genres, CBF_GENRE_DATA, kernels)
+    
     anime_indexes = pd.Series(cbf_matrix.index, index=cbf_matrix['Name'])
     if name == "":
         name = np.random.choice(anime_indexes.index)
     try:
-        similarity_scores = sorted(
-            list(enumerate(similarity_matrix[anime_indexes[name]]))
-            , key= lambda x: x[1]
-            , reverse= True)
-        similarity_scores = list(filter(lambda x: x[0] != anime_indexes[name]
-                                        , similarity_scores))
+        similarity_scores = get_scores(similarity_matrix, anime_indexes, name)
 
         ind = similarity_scores[0: count_recomendations]
         recomendations = []
         
         for _, index in enumerate(ind):
-            recomendations.append([cbf_matrix['Name'].iloc[index[0]], index[1]])
+            title = cbf_matrix[['Name', 'Genres', 'sypnopsis']].iloc[index[0]].tolist()
+            title.append(index[1])
+            recomendations.append(title)
 
         return name, recomendations
     except:
@@ -84,31 +90,62 @@ def make_recomendations_with_sypnopsis(cbf_matrix, name = ""
     if os.path.exists(CBF_SYPNOPSIS_DATA) and kernels is cosine_similarity:
         similarity_matrix = np.load(CBF_SYPNOPSIS_DATA)
     else:
-        
         synopsis = cbf_matrix['sypnopsis'].str.strip(',.!?:"()').str.split(' ').astype(str)
-        tfidfv = TfidfVectorizer()
-        tfidf_sypnosis = tfidfv.fit_transform(synopsis)
-        similarity_matrix = kernels(tfidf_sypnosis, tfidf_sypnosis)
-
-        if kernels is cosine_similarity:
-            np.save(CBF_SYPNOPSIS_DATA, similarity_matrix)
+        similarity_matrix = vectorization(synopsis, CBF_SYPNOPSIS_DATA, kernels)
 
     anime_indexes = pd.Series(cbf_matrix.index, index=cbf_matrix['Name'])
     if name == "":
         name = np.random.choice(anime_indexes.index)
     try:
-        similarity_scores = sorted(
-            list(enumerate(similarity_matrix[anime_indexes[name]]))
-            , key= lambda x: x[1]
-            , reverse= True)
-        similarity_scores = list(filter(lambda x: x[0] != anime_indexes[name]
-                                            , similarity_scores))
+        similarity_scores = get_scores(similarity_matrix, anime_indexes, name)
 
         ind = similarity_scores[0: count_recomendations]
         recomendations = []
 
         for _, index in enumerate(ind):
-            recomendations.append([cbf_matrix['Name'].iloc[index[0]], index[1]])
+            title = cbf_matrix[['Name', 'Genres', 'sypnopsis']].iloc[index[0]].tolist()
+            title.append(index[1])
+            recomendations.append(title)
+
+        return name, recomendations
+    except:
+        return None
+    
+def make_recomendations_with_genres_and_sypnopsis(cbf_matrix, name = ""
+                                                  , count_recomendations = 10
+                                                  , kernels= cosine_similarity):
+    if os.path.exists(CBF_SYPNOPSIS_DATA) and kernels is cosine_similarity:
+        similarity_sypnonpsis_matrix = np.load(CBF_SYPNOPSIS_DATA)
+    else:
+        synopsis = cbf_matrix['sypnopsis'].str.strip(',.!?:"()').str.split(' ').astype(str)
+        similarity_sypnonpsis_matrix = vectorization(synopsis
+                                                     , CBF_SYPNOPSIS_DATA
+                                                     , kernels)
+
+    if os.path.exists(CBF_GENRE_DATA) and kernels is cosine_similarity:
+        similarity_genres_matrix = np.load(CBF_GENRE_DATA)
+    else:
+        genres = cbf_matrix['Genres'].str.split(', ').astype(str)
+        similarity_genres_matrix = vectorization(genres
+                                                 , CBF_GENRE_DATA, kernels)
+    anime_indexes = pd.Series(cbf_matrix.index, index=cbf_matrix['Name'])
+    if name == "":
+        name = np.random.choice(anime_indexes.index)
+    try:
+        similarity_genres_scores = get_scores(similarity_genres_matrix
+                                                , anime_indexes, name, False)
+        similarity_sypnopsis_scores = get_scores(similarity_sypnonpsis_matrix
+                                                    , anime_indexes, name, False)
+        similarity_scores = [[x[0], x[1] * y[1]] for x,y in zip(similarity_genres_scores,similarity_sypnopsis_scores)]
+        similarity_scores = sorted(similarity_scores
+                                , key= lambda x: x[1], reverse= True)
+        ind = similarity_scores[0: count_recomendations]
+        recomendations = []
+
+        for _, index in enumerate(ind):
+            title = cbf_matrix[['Name', 'Genres', 'sypnopsis']].iloc[index[0]].tolist()
+            title.append(index[1])
+            recomendations.append(title)
 
         return name, recomendations
     except:
--- app/views.py
+++ app/views.py
@@ -1,11 +1,18 @@
 import pandas as pd
 
-from flask import render_template
+from flask import render_template, redirect
 from app import app
 from app.source import *
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField
+from wtforms.validators import DataRequired
+from app.recomendations import make_recomendations_with_genre, make_recomendations_with_sypnopsis
 
-sypnopsis_data = pd.read_csv(ANIME_DIR + 'anime_with_synopsis.csv'
-                             , index_col='MAL_ID')
+class TitleForm():
+    title = StringField('title', validators=[DataRequired()])
+    count = IntegerField('count', validators=[DataRequired()])
+
+sypnopsis_data = pd.read_csv(ANIME_DIR + 'anime_with_synopsis.csv')
 dict = {'Unknown' : 0}
 sypnopsis_data['Score'] = sypnopsis_data['Score'].apply(lambda x : dict[x] if x == 'Unknown' else x).astype(float)
 
@@ -15,5 +22,36 @@ def index():
 
 @app.route('/recomendations/')
 def recomendations():
-    top_10 = sypnopsis_data.sort_values(by= 'Score', ascending=False).head(10).values.tolist()
+    top_10 = sypnopsis_data.sort_values(by= 'Score', ascending= False).head(10)[['Name', 'Genres', 'sypnopsis']].values.tolist()
     return render_template("recomendations.html", titles = top_10)
+
+@app.route('/recomendations/<title>')
+def content_based_recomendations(title):
+    title = title.replace('_', ' ')
+    recomendations = make_recomendations_with_sypnopsis(sypnopsis_data
+                                                        , title)
+    if recomendations is None:
+        return render_template("empty.html", title= title)
+    else:
+
+        return render_template("recomendations.html"
+                               , title= recomendations[0]
+                               , titles= recomendations[1]
+                               , count= 10)
+
+@app.route('/recomendations/<title>/<count>')
+def content_based_recomendations_with_count(title, count):
+    if count is None:
+        return redirect('/recomendations/title')
+    title = title.replace('_', ' ')
+    recomendations = make_recomendations_with_sypnopsis(sypnopsis_data
+                                                        , title
+                                                        , int(count))
+    if recomendations is None:
+        return render_template("empty.html", title= title)
+    else:
+
+        return render_template("recomendations.html"
+                               , title= recomendations[0]
+                               , titles= recomendations[1]
+                               , count= count)
